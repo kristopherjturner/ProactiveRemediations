fe9203a4b5046ae00d03f4a5434837a63636daed
Kristopher Turner <13710532+kristopherjturner@users.noreply.github.com>
2022-12-30 13:52:14 -0600

    Changes

--- Detect-MappedDrives.ps1
+++ Detect-MappedDrives.ps1
@@ -11,42 +11,32 @@
 
 #>
 
-$ShareName = "azurefileharename"
-$StorageAccount = "storageaccount"
+$ShareName = "Documentation"
+$StorageAccount = "pnwfs10fileshare"
+
 
 $Date = Get-Date -UFormat "%Y-%m-%d_%H-%m-%S"
 $LogFileName = "DriveMapping-" + "Detect-" + $ShareName + "-" + $date + ".log"
 Start-Transcript -Path $(Join-Path $env:temp $LogFileName)
 
-try {
-	$Path = "\\$StorageAccount.file.core.windows.net\$ShareName"
-	$ProviderName = Get-WmiObject win32_logicaldisk | select-object ProviderName | where-object { $_.ProviderName -eq "$Path" }
-	if ($ProviderName) {
-		Write-Host $Path ("already exist.")
-		Exit 0
-	}
-	else {
-		Write-Host ("Current mapped drive doesn't exist.")
-		Exit 1
-	}	
 
-	<#
-	$connectTestResult = Test-NetConnection -ComputerName "$storageaccount.file.core.windows.net" -Port 445
-	if ($connectTestResult.TcpTestSucceeded) {
-		# Continue With script
-		Write-Host ("Connection to storage account via 443 succesful.")
-		Exit 1
-	}
-	else {
-		Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
+$Path = "\\$StorageAccount.file.core.windows.net\$ShareName"
+
+# For troubleshooting
+$PSDrive = Get-PSDrive | Where-Object{$_.DisplayRoot -eq $path}
+$PSDrive
+
+try {
+	if (Get-PSDrive | Where-Object{$_.DisplayRoot -eq $path}) {
+		Write-Host "Drive is mapped"
 		Exit 0
+	} else {
+		Write-Host "Drive is not mapped"
 	}
-	#>
 }
 catch {
 	$errMsg = $_.Exception.Message
-	Write-Error $errMsg
-	exit 1
+	Write-Host $errMsg
+	Exit 1
 }
-
 Stop-Transcript
