2073b19bff26e618a35db3e2d52be4e933a2218f
Kristopher Turner <13710532+kristopherjturner@users.noreply.github.com>
2023-01-02 09:28:20 -0600

    update script

--- Detect-MappedDrives.ps1
+++ Detect-MappedDrives.ps1
@@ -1,4 +1,17 @@
-$ShareName = "DMS"
+<#
+.DESCRIPTION
+	Detects if user already has Azure File Share mapped.  If detected it will exit script and not run remediation script.
+	If not detected it will then detected if user can connect to Azure File Share via port 445. If user can connect via port 445 the remediation
+	script will run. If user can't connect it will exit.
+
+
+.NOTES
+	Author and Edited: Kristopher Turner (InvokeLLC)
+	Make sure to change change variables!!!
+
+#>
+
+$ShareName = "audittestomid"
 $StorageAccount = "pnwfs10fileshare"
 
 
@@ -6,42 +19,19 @@ $Date = Get-Date -UFormat "%Y-%m-%d_%H-%m-%S"
 $LogFileName = "DriveMapping-" + "Detect-" + $ShareName + "-" + $date + ".log"
 Start-Transcript -Path $(Join-Path $env:temp $LogFileName)
 
+$RegistryKey = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\MountPoints2\##$storageaccount.file.core.windows.net#$sharename"
 
-$Path = "\\$StorageAccount.file.core.windows.net\$ShareName"
+Write-Host "Looking to see if registry key exist."
+$RegistryKey
 
-function Test-RunningAsSystem {
-    [CmdletBinding()]
-    param()
-    process {
-        return [bool]($(whoami -user) -match "S-1-5-18")
-    }
+# For troubleshooting
+
+try {
+	if(-NOT (Test-Path -LiteralPath "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\MountPoints2\##$storageaccount.file.core.windows.net#$sharename")){ Exit 1 };
 }
+catch { 
+	Write-Warning "Drive Not Mapped"
+	Exit 1 }
+Exit 0
 
-if (-not (Test-RunningAsSystem)) {
-
-    # For troubleshooting
-    $PSDrive = Get-PSDrive | Where-Object { $_.DisplayRoot -eq $path }
-    $PSDrive
-    Write-Output "Looking for mapped drive" $Path
-    Write-Output "Listing all mapped drives on system."
-    Get-PSDrive -PSProvider FileSystem
-
-    Get-PSDrive | Where-Object { $_.DisplayRoot -eq $path }
-
-
-    try {
-        if (Get-PSDrive | Where-Object { $_.DisplayRoot -eq $Path }) {
-            Write-Host "Drive is mapped"
-            Exit 0
-        }
-        else {
-            Write-Host "Drive is not mapped"
-        }
-    }
-    catch {
-        $errMsg = $_.Exception.Message
-        Write-Host $errMsg
-        Exit 1
-    }
-}   
 Stop-Transcript
--- Old/Detect-MappedDrives.ps1
+++ Old/Detect-MappedDrives.ps1
@@ -1,17 +1,4 @@
-<#
-.DESCRIPTION
-	Detects if user already has Azure File Share mapped.  If detected it will exit script and not run remediation script.
-	If not detected it will then detected if user can connect to Azure File Share via port 445. If user can connect via port 445 the remediation
-	script will run. If user can't connect it will exit.
-
-
-.NOTES
-	Author and Edited: Kristopher Turner (InvokeLLC)
-	Make sure to change change variables!!!
-
-#>
-
-$ShareName = "Documentation"
+$ShareName = "DMS"
 $StorageAccount = "pnwfs10fileshare"
 
 
@@ -22,21 +9,39 @@ Start-Transcript -Path $(Join-Path $env:temp $LogFileName)
 
 $Path = "\\$StorageAccount.file.core.windows.net\$ShareName"
 
-# For troubleshooting
-$PSDrive = Get-PSDrive | Where-Object{$_.DisplayRoot -eq $path}
-$PSDrive
+function Test-RunningAsSystem {
+    [CmdletBinding()]
+    param()
+    process {
+        return [bool]($(whoami -user) -match "S-1-5-18")
+    }
+}
 
-try {
-	if (Get-PSDrive | Where-Object{$_.DisplayRoot -eq $path}) {
-		Write-Host "Drive is mapped"
-		Exit 0
-	} else {
-		Write-Host "Drive is not mapped"
-	}
-}
-catch {
-	$errMsg = $_.Exception.Message
-	Write-Host $errMsg
-	Exit 1
-}
+if (-not (Test-RunningAsSystem)) {
+
+    # For troubleshooting
+    $PSDrive = Get-PSDrive | Where-Object { $_.DisplayRoot -eq $path }
+    $PSDrive
+    Write-Output "Looking for mapped drive" $Path
+    Write-Output "Listing all mapped drives on system."
+    Get-PSDrive -PSProvider FileSystem
+
+    Get-PSDrive | Where-Object { $_.DisplayRoot -eq $path }
+
+
+    try {
+        if (Get-PSDrive | Where-Object { $_.DisplayRoot -eq $Path }) {
+            Write-Host "Drive is mapped"
+            Exit 0
+        }
+        else {
+            Write-Host "Drive is not mapped"
+        }
+    }
+    catch {
+        $errMsg = $_.Exception.Message
+        Write-Host $errMsg
+        Exit 1
+    }
+}   
 Stop-Transcript
