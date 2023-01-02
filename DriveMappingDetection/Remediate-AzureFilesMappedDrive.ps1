d0ee6ae0efcec5a2f56af3fb45688d61e1bc9124
Kristopher Turner <13710532+kristopherjturner@users.noreply.github.com>
2023-01-02 17:56:01 -0600

    Updates

--- DriveMappingDetection/Remediate-AzureFilesMappedDrive.ps1
+++ DriveMappingDetection/Remediate-AzureFilesMappedDrive.ps1
@@ -9,37 +9,34 @@
 
 #>
 
-$ShareName = "documentation"
-$DriveLabel = "Documentation"
-$StorageAccount = "pnwfs10fileshare"
+$ShareName = "" # - Azure File Name
+$DriveLabel = "" # - Drive Label
+$StorageAccount = "" # - Storage Account
 
 $Date = Get-Date -UFormat "%Y-%m-%d_%H-%m-%S"
 $LogFileName = "DriveMapping-" + "Remediate-" + $ShareName + $date + ".log"
 Start-Transcript -Path $(Join-Path $env:temp $LogFileName)
 
-Write-Host ("Share Name:")$ShareName
-Write-Host ("Drive Label:")$DriveLabel
-Write-Host ("Storage Account:")$StorageAccount
-
 function Test-RunningAsSystem {
-	[CmdletBinding()]
-	param()
-	process {
-		return [bool]($(whoami -user) -match "S-1-5-18")
-	}
+    [CmdletBinding()]
+    param()
+    process {
+        return [bool]($(whoami -user) -match "S-1-5-18")
+    }
 }
 
 $Path = "\\$StorageAccount.file.core.windows.net\$ShareName"
-$ProviderName = Get-WmiObject win32_logicaldisk | select-object ProviderName | where-object {$_.ProviderName -eq "$Path"}
-$DriveLetter = (68..90 | ForEach-Object{$L=[char]$_; if ((Get-PSDrive).Name -notContains $L) {$L}})[0]
+$ProviderName = Get-WmiObject win32_logicaldisk | select-object ProviderName | where-object { $_.ProviderName -eq "$Path" }
+$DriveLetter = (68..90 | ForEach-Object { $L = [char]$_; if ((Get-PSDrive).Name -notContains $L) { $L } })[0]
 Write-Host ("$DriveLetter is next available.")
 
 if ($ProviderName) {
-	Write-Host $Path ("already exist.")
-	Write-Host ("Exiting Script")
-	Exit 1
-} else {
-	Write-Host ("Mapped drive will continue.")
+    Write-Host $Path ("already exist.")
+    Write-Host ("Exiting Script")
+    Exit
+}
+else {
+    Write-Host ("Mapped drive will continue.")
 }
 
 
@@ -47,71 +44,72 @@ $connectTestResult = Test-NetConnection -ComputerName "$storageaccount.file.core
 if ($connectTestResult.TcpTestSucceeded) {
     Write-Host ("Connection to storage account via 443 succesful. Script will continue.")
 
+    else {
+        Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
+        Write-Host ("Skipping drive creation due to connectivity.")
+        Exit
+    }
+}
 
-    if (-not (Test-RunningAsSystem)) {
 
-        $psDrives = Get-PSDrive | Where-Object { $_.Provider.Name -eq "FileSystem" -and $_.Root -notin @("$env:SystemDrive\", "D:\") } `
-        | Select-Object @{N = "DriveLetter"; E = { $_.Name } }, @{N = "Path"; E = { $_.DisplayRoot } }
+if (-not (Test-RunningAsSystem)) {
 
-        try {
-            #check if variable in unc path exists, e.g. for $env:USERNAME -> resolving
-            if ($Path -match '\$env:') {
-                $Path = $ExecutionContext.InvokeCommand.ExpandString($Path)
-            }
+    $psDrives = Get-PSDrive | Where-Object { $_.Provider.Name -eq "FileSystem" -and $_.Root -notin @("$env:SystemDrive\", "D:\") } `
+    | Select-Object @{N = "DriveLetter"; E = { $_.Name } }, @{N = "Path"; E = { $_.DisplayRoot } }
 
-            #if label is null we need to set it to empty in order to avoid error
-            if ($null -eq $DriveLabel) {
-                $DriveLabel = ""
-            }
+    try {
+        #check if variable in unc path exists, e.g. for $env:USERNAME -> resolving
+        if ($Path -match '\$env:') {
+            $Path = $ExecutionContext.InvokeCommand.ExpandString($Path)
+        }
 
-            $exists = $psDrives | Where-Object { $_.Path -eq $Path -or $_.DriveLetter -eq $DriveLetter }
-            $process = $true
+        #if label is null we need to set it to empty in order to avoid error
+        if ($null -eq $DriveLabel) {
+            $DriveLabel = ""
+        }
 
-            if ($null -ne $exists -and $($exists.Path -eq $Path -and $exists.DriveLetter -eq $DriveLetter )) {
-                Write-Output "Drive '$($DriveLetter):\' '$($Path)' already exists with correct Drive Letter and Path"
-                $process = $false
+        $exists = $psDrives | Where-Object { $_.Path -eq $Path -or $_.DriveLetter -eq $DriveLetter }
+        $process = $true
 
-            }
-            else {
-                # Mapped with wrong config -> Delete it
-                Get-PSDrive | Where-Object { $_.DisplayRoot -eq $Path -or $_.Name -eq $DriveLetter } | Remove-PSDrive -EA SilentlyContinue
-            }
+        if ($null -ne $exists -and $($exists.Path -eq $Path -and $exists.DriveLetter -eq $DriveLetter )) {
+            Write-Output "Drive '$($DriveLetter):\' '$($Path)' already exists with correct Drive Letter and Path"
+            $process = $false
 
-            if ($process) {
-                Write-Output "Mapping network drive $($Path)"
-                $null = New-PSDrive -PSProvider FileSystem -Name $DriveLetter -Root $Path -Description $DriveLabel -Persist -Scope global -EA SilentlyContinue
+        }
+        else {
+            # Mapped with wrong config -> Delete it
+            Get-PSDrive | Where-Object { $_.DisplayRoot -eq $Path -or $_.Name -eq $DriveLetter } | Remove-PSDrive -EA SilentlyContinue
+        }
+
+        if ($process) {
+            Write-Output "Mapping network drive $($Path)"
+            $null = New-PSDrive -PSProvider FileSystem -Name $DriveLetter -Root $Path -Description $DriveLabel -Persist -Scope global -EA SilentlyContinue
 				(New-Object -ComObject Shell.Application).NameSpace("$($DriveLetter):").Self.Name = $DriveLabel
-            }
-        }
-        catch {
-            $available = Test-Path $($Path)
-            if (-not $available) {
-                Write-Error "Unable to access path '$($Path)' verify permissions and authentication!"
-            }
-            else {
-                Write-Error $_.Exception.Message
-                Exit 1
-            }
         }
     }
-
-    # Remove unassigned drives
-    if ($removeStaleDrives -and $null -ne $psDrives) {
-        $diff = Compare-Object -ReferenceObject $driveMappingConfig -DifferenceObject $psDrives -Property "DriveLetter" -PassThru | Where-Object { $_.SideIndicator -eq "=>" }
-        foreach ($unassignedDrive in $diff) {
-            Write-Warning "Drive '$($unassignedDrive.DriveLetter)' has not been assigned - removing it..."
-            Remove-SmbMapping -LocalPath "$($unassignedDrive.DriveLetter):" -Force -UpdateProfile
+    catch {
+        $available = Test-Path $($Path)
+        if (-not $available) {
+            Write-Error "Unable to access path '$($Path)' verify permissions and authentication!"
+        }
+        else {
+            Write-Error $_.Exception.Message
+            Exit 1
         }
     }
+}
 
-    # Fix to ensure drives are mapped as persistent!
-    $null = Get-ChildItem -Path HKCU:\Network -ErrorAction SilentlyContinue | ForEach-Object { New-ItemProperty -Name ConnectionType -Value 1 -Path $_.PSPath -Force -ErrorAction SilentlyContinue }
+# Remove unassigned drives
+if ($removeStaleDrives -and $null -ne $psDrives) {
+    $diff = Compare-Object -ReferenceObject $driveMappingConfig -DifferenceObject $psDrives -Property "DriveLetter" -PassThru | Where-Object { $_.SideIndicator -eq "=>" }
+    foreach ($unassignedDrive in $diff) {
+        Write-Warning "Drive '$($unassignedDrive.DriveLetter)' has not been assigned - removing it..."
+        Remove-SmbMapping -LocalPath "$($unassignedDrive.DriveLetter):" -Force -UpdateProfile
+    }
 }
-else {
-    Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
-    Write-Host ("Skipping drive creation due to connectivity.")
-    Exit 1
-}
+
+# Fix to ensure drives are mapped as persistent!
+$null = Get-ChildItem -Path HKCU:\Network -ErrorAction SilentlyContinue | ForEach-Object { New-ItemProperty -Name ConnectionType -Value 1 -Path $_.PSPath -Force -ErrorAction SilentlyContinue }
 
 Stop-Transcript
 
