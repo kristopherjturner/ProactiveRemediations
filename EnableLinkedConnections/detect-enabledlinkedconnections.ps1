efdbea7ae8d221e00232e0e104bc7ab7c8ab8766
Kristopher Turner <13710532+kristopherjturner@users.noreply.github.com>
2023-01-02 14:14:34 -0600

    updates

--- EnableLinkedConnections/detect-enabledlinkedconnections.ps1
+++ EnableLinkedConnections/detect-enabledlinkedconnections.ps1
@@ -1,6 +1,26 @@
+<#
+.DESCRIPTION
+	Detects if Registry Key and Value exist.  If True then script will end and compliant.  If false it will end with Not compliant and trigger the remediation script.
+
+
+.NOTES
+	Author and Edited: Kristopher Turner (InvokeLLC)
+	
+#>
+
+
+$Date = Get-Date -UFormat "%Y-%m-%d_%H-%m-%S"
+$LogFileName = "Detect-" + "EnableLinkedConnections-" + $date + ".log"
+Start-Transcript -Path $(Join-Path $env:temp $LogFileName)
+
+
 try {
-	if(-NOT (Test-Path -LiteralPath "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System")){ Exit 1 };
-	if((Get-ItemPropertyValue -LiteralPath 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' -Name 'EnableLinkedConnections' -ea SilentlyContinue) -eq 0) {  } else { Exit 1 };
+	if (-NOT (Test-Path -LiteralPath "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System")) { Exit 1 };
+	if ((Get-ItemPropertyValue -LiteralPath 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' -Name 'EnableLinkedConnections' -ea SilentlyContinue) -eq 0) {  } else { Exit 1 }
 }
-catch { exit 1 }
-return $true
+catch { 
+	Write-Warning "Not Compliant"
+	exit 1 
+}
+
+Stop-Transcript
--- EnableLinkedConnections/remediate-enablelinkedconnections.ps1
+++ EnableLinkedConnections/remediate-enablelinkedconnections.ps1
@@ -1,2 +1,21 @@
+<#
+.DESCRIPTION
+	Remediates EnableLinkedConnection Registry Value from 0 to 1.
+
+
+.NOTES
+	Author and Edited: Kristopher Turner (InvokeLLC)
+	
+#>
+
+
+$Date = Get-Date -UFormat "%Y-%m-%d_%H-%m-%S"
+$LogFileName = "Remediate-" + "EnableLinkedConnections-" + $date + ".log"
+Start-Transcript -Path $(Join-Path $env:temp $LogFileName)
+
+
 if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System") -ne $true) {  New-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -force -ea SilentlyContinue };
 New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' -Name 'EnableLinkedConnections' -Value 1 -PropertyType DWord -Force -ea SilentlyContinue;
+
+
+Stop-Transcript
