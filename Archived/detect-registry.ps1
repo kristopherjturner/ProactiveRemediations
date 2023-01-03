1c0ffd66b7cab8b50861f4b46bb7bb1cb33fd676
Kristopher Turner <13710532+kristopherjturner@users.noreply.github.com>
2023-01-02 18:30:39 -0600

    updates

--- Archived/detect-registry.ps1
+++
@@ -1,6 +0,0 @@
-# Reg2CI (c) 2022 by Roger Zander
-try {
-	if(-NOT (Test-Path -LiteralPath "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\MountPoints2\##pnwfs10fileshare.file.core.windows.net#audittestomid")){ return $false };
-}
-catch { return $false }
-return $true
